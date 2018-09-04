13a4ab090be3d5446168b800836067ac8b0aa323
justnpT <Michal.Nowierski@gmail.com>
2018-09-05 01:08:00 +0200

    add assert

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -10,5 +11,6 @@ public class Test {
         driver.get("https://www.google.com");
         System.out.print("cos");
         driver.quit();
+        Assert.assertTrue(false);
     }
 }
